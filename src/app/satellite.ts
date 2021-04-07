af97737e2da92b3d0bf7f7b32afcba081f609b58
Hank Hantak <hank.hantak@outlook.com>
2021-04-07 17:10:29 -0500

    major changes with a lot of copy/paste from book

---
+++ src/app/satellite.ts
@@ -0,0 +1,25 @@
+export class Satellite {
+name: string;
+type: string;
+launchDate: string;
+orbitType: string;
+operational: boolean;
+
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+  this.name = name;
+  this.type = type;
+  this.launchDate = launchDate;
+  this.orbitType = orbitType; 
+  this.operational = operational;
+  }
+
+  shouldShowWarning(): boolean {
+    if (this.type.toUpperCase() === "SPACE DEBRIS") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+
